9de38fda6baf626062d0c6659c6fa40ecf7840af
Kevin <jistrogames@gmail.com>
2023-02-01 19:08:22 -0600

    Múltiples actividades

--- 03 Trabajando Multiples Actividades/DevolucionResOtras/app/src/main/java/com/example/devolucionresotras/MainActivity.java
+++ 03 Trabajando Multiples Actividades/DevolucionResOtras/app/src/main/java/com/example/devolucionresotras/MainActivity.java
@@ -2,13 +2,44 @@ package com.example.devolucionresotras;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener
+{
+    private static final int REQUEST_CODE = 1000;
+    Button b;
+    TextView t;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        b = (Button) findViewById(R.id.button);
+        t = (TextView) findViewById(R.id.textView);
+        b.setOnClickListener(this);
+    }
+
+
+    public void onClick (View v)
+    {
+        Intent intent = new Intent(this, SecondActivity.class);
+        startActivityForResult(intent, REQUEST_CODE);
+    }
+
+    @Override
+    protected void onActivityResult (int requestCode, int resultCode, Intent data)
+    {
+        super.onActivityResult(requestCode, resultCode, data);
+        if ((requestCode == REQUEST_CODE) && (resultCode == Activity.RESULT_OK))
+        {
+            t.setText(data.getStringExtra("secondActivity"));
+        }
     }
 }
--- 03 Trabajando Multiples Actividades/DevolucionResOtras/app/src/main/java/com/example/devolucionresotras/SecondActivity.java
+++ 03 Trabajando Multiples Actividades/DevolucionResOtras/app/src/main/java/com/example/devolucionresotras/SecondActivity.java
@@ -2,13 +2,37 @@ package com.example.devolucionresotras;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class SecondActivity extends AppCompatActivity {
+public class SecondActivity extends AppCompatActivity 
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) 
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        Button b = (Button) findViewById(R.id.button2);
+        b.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v) 
+            {
+                Intent intent = new Intent();
+                EditText e = (EditText) findViewById(R.id.editText);
+                String data = e.getText().toString();
+                intent.putExtra("secondActivity", data);
+                setResult(Activity.RESULT_OK, intent);
+                finish();
+            }
+        });
     }
+
 }
