b3bfa252e07d0a51963b7d2deffea5274bdb8d94
Kevin <jistrogames@gmail.com>
2023-02-08 18:04:24 -0600

    Elementos de la ui FINAL

--- 04Elementos de la IU/fragmentos2/app/src/main/java/com/example/fragmentos2/MainActivity.java
+++ 04Elementos de la IU/fragmentos2/app/src/main/java/com/example/fragmentos2/MainActivity.java
@@ -1,14 +1,25 @@
 package com.example.fragmentos2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity 
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) 
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FragmentA f = new FragmentA();
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        ft.add(R.id.frag_placeholder, f, "");
+        ft.commit();
     }
 }
