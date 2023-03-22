da269c0b3b018603e89c36c5da163307f862a29f
Kevin <kevin.padilla.islas@proton.me>
2023-03-22 08:48:12 -0600

    avance actividad 12

---
+++ 12 Comprension Arbol de widgets/widget_tree/lib/main.dart
@@ -0,0 +1,131 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("widget tree"),
+      ),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Column(
+              children: <Widget>[
+                Row (
+                  children: <Widget>[
+                    Container(
+                      color: Colors.yellow,
+                      height: 40.0,
+                      width: 40.0,
+                    ),
+                    Padding(padding: EdgeInsets.all(16.0),),
+                    Expanded(
+                      child: Container(
+                        color: Colors.amber,
+                        height: 40.0,
+                        width: 40.0,
+                      ),
+                    ),
+                    Padding(padding: EdgeInsets.all(16.0),),
+                    Container(
+                      color: Colors.brown,
+                      height: 40.0,
+                      width: 40.0,
+                    ),
+                  ],
+                ),
+                Padding(padding: EdgeInsets.all(16.0),),
+                Row(
+                  children: <Widget>[
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisSize: MainAxisSize.max,
+                      children: <Widget>[
+                        Container(
+                          color: Colors.yellow,
+                          height: 40.0,
+                          width: 40.0,
+                        ),
+                        Padding(padding: EdgeInsets.all(16.0),),
+                        Container(
+                          color: Colors.amber,
+                          height: 40.0,
+                          width: 40.0,
+                        ),
+                        Padding(padding: EdgeInsets.all(16.0),),
+                        Container(
+                          color: Colors.brown,
+                          height: 20.0,
+                          width: 20.0,
+                        ),
+                        Divider(),
+                        Row(
+                          children: <Widget>[
+                            CircleAvatar(
+                              backgroundColor: Colors.lightGreen,
+                              radius: 100.0,
+                              child: Stack(
+                                children: <Widget>[
+                                  Container(
+                                    height: 100.0,
+                                    width: 100.0,
+                                    color: Colors.yellow,
+                                  ),
+                                  Container(
+                                    height: 60.0,
+                                    width: 60.0,
+                                    color: Colors.amber,
+                                  ),
+                                  Container(
+                                    height: 40.0,
+                                    width: 40.0,
+                                    color: Colors.brown,
+                                  ),
+                                ]
+                              )
+                            ),
+                          ],
+                        ),
+                        Divider(),
+                        Text('End of the line'),
+                      ],
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
